aba42da62cd4125acaf6fcdfa1710cd10c2f10be
Nicholas Faiz <nicholas.faiz@gmail.com>
2010-09-27 15:54:27 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ada::Application.config.secret_token = 'c12f692a4d6cd623814a1b25c002f8e64dfa0f5db5d4ae798d2edc61f05210fb6ed36ea0b5c37b22b293cedf86bb886b1a67eec02690fa95aec3d1db58606d1a'
