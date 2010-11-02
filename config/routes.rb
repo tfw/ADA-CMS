d820a5b2537f9d8e88fdf1265eaa2afd45a46924
biv <nicholas.faiz@gmail.com>
2010-11-03 09:14:06 +1100

    use staff/home not /home for dasboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ada::Application.routes.draw do
-  match 'home', :to => 'inkling/home#dashboard', :as => "user_root"  
-  match 'home', :to => 'inkling/home#dashboard', :as => "root"
+  match 'staff/home', :to => 'inkling/home#dashboard', :as => "user_root"  
+  match 'staff/home', :to => 'inkling/home#dashboard', :as => "root"
 
   devise_scope :inkling_user do
     get "login", :to => "devise/sessions#new"
