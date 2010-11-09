44acc22ec4cdd3e2fed7fdf1a7a3d6968498018a
biv <nicholas.faiz@gmail.com>
2010-11-10 09:47:55 +1100

    adding jquery

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,7 @@ class PagesController < ContentController
   
   def show
     @page = Page.find_by_id(params[:id])
-    # @archive = @page.archive
-    # @archive ||= ADAArchive.new #see lib/ada_archive.rb
-    # 
-    # @parent_pages = Page.find_all_by_archive_id_and_parent_id(@page.archive.id, nil)
-    # # @children_pages = Page.find_all_by_archive_id_and_parent_id(@page.archive.id, )
-    # respond_with()
+    debugger
+    respond_with(@page)
   end
 end
