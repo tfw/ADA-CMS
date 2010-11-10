12eea35e206c3cee7f7110e5a9b9a3aa2219f832
biv <nicholas.faiz@gmail.com>
2010-11-10 20:47:42 +1100

    use ada_archive dummy object for ada archive

--- app/controllers/staff/pages_controller.rb
+++ app/controllers/staff/pages_controller.rb
@@ -1,3 +1,4 @@
+require 'ada_archive'
 class Staff::PagesController < Inkling::BaseController
   inherit_resources                                                                                     
   defaults :resource_class => Page, :instance_name => 'page'
