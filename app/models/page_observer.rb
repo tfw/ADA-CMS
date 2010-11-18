ebf4a02b06734bce12cf19bbe94c4c2c72a99b2f
Nicholas Faiz <nicholas.faiz@gmail.com>
2010-11-19 01:54:54 +1100

    page_observer now writes links

--- app/models/page_observer.rb
+++ app/models/page_observer.rb
@@ -1,5 +1,6 @@
 class PageObserver < ActiveRecord::Observer
-   # include ActionController::UrlFor
+   # include ActionView::Helpers::UrlHelper
+   include ActionController::UrlWriter
    include Rails.application.routes.url_helpers
    
   def after_create(page)
@@ -16,7 +17,6 @@ class PageObserver < ActiveRecord::Observer
   
   private
   def log(verb, page)
-    puts " **** #{url_for page.title} \n\n"
-    Inkling::Log.create!(:text => "#{page.author.email} #{verb} page #{url_for page.title}")
+    Inkling::Log.create!(:text => "#{page.author.email} #{verb} page <a href='#{edit_staff_page_path(page)}'>page.title</a>")
   end
 end
