620f1a108cae83b711947dc93ed13684e5ca1ed4
biv <nicholas.faiz@gmail.com>
2010-11-18 22:36:35 +1100

    page_observer_spec finished

--- spec/models/page_observer_spec.rb
+++ spec/models/page_observer_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-include Inkling::Slugs
 
 describe PageObserver do
 
@@ -7,7 +6,21 @@ describe PageObserver do
     it "saves a log after save" do
       page = Page.make
       Inkling::Log.all.size > 0
-      Inkling::Log.first.text.has_content "created page #{page.title}"
+      Inkling::Log.first.text.should =~ /created page #{page.title}/
+    end
+    
+    it "saves a log after update" do
+      page = Page.make
+      page.body = "changed!"
+      Inkling::Log.all.size > 0
+      Inkling::Log.last.text.should =~ /edited page #{page.title}/
+    end
+    
+    it "saves a log after destroy" do
+      page = Page.make
+      page.destroy
+      Inkling::Log.all.size > 0
+      Inkling::Log.last.text.should =~ /deleted page #{page.title}/
     end
   end
 end
