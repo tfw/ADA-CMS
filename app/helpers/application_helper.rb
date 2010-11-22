dd150f3a8d42eec9cb9c9d94b0115f0066361502
biv <nicholas.faiz@gmail.com>
2010-11-22 13:01:27 +1100

    elementary placeholder icons, tab for dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def archive_css(page)
     page.archive.nil? ? 'default' : page.archive.name.gsub(" ", "").underscore
   end
+  
+  def placeholder_icon
+    icon = rand(3) + 1
+    image_tag("placeholders/#{icon}.png", :size => "20x20", :class => "placeholder-icon")
+  end
 end
