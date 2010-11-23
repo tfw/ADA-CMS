dabb5308ed6bd616d55f52d5fcf820e360c2c7c1
Gus Gollings <gus.gollings@gmail.com>
2010-11-23 13:52:08 +1100

    good idea to have this as a backup - has saved people in the past?!

---
+++ db/schema.rb
@@ -0,0 +1,139 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20101027001044) do
+
+  create_table "archives", :force => true do |t|
+    t.string   "name",       :null => false
+    t.string   "slug",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "dataset_entries", :force => true do |t|
+    t.integer  "dataset_id"
+    t.string   "key"
+    t.text     "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "dataset_related_materials", :force => true do |t|
+    t.integer  "dataset_id"
+    t.string   "uri"
+    t.string   "label"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "datasets", :force => true do |t|
+    t.string   "label"
+    t.string   "resource"
+    t.string   "study3"
+    t.string   "about"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_can_can_actions", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_logs", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "text",       :null => false
+  end
+
+  create_table "inkling_paths", :force => true do |t|
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.string   "slug",         :null => false
+    t.integer  "content_id"
+    t.string   "content_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_permissions", :force => true do |t|
+    t.integer  "type_id"
+    t.integer  "role_id",           :null => false
+    t.integer  "can_can_action_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_role_memberships", :force => true do |t|
+    t.integer "user_id", :null => false
+    t.integer "role_id", :null => false
+  end
+
+  create_table "inkling_roles", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_themes", :force => true do |t|
+    t.string   "name",                                :null => false
+    t.text     "body"
+    t.string   "extension",  :default => ".html.erb", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_types", :force => true do |t|
+    t.string   "klass_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inkling_users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "title",        :null => false
+    t.string   "link_title",   :null => false
+    t.text     "description"
+    t.integer  "author_id",    :null => false
+    t.text     "body"
+    t.text     "breakout_box"
+    t.integer  "parent_id"
+    t.integer  "archive_id"
+    t.string   "partial"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
