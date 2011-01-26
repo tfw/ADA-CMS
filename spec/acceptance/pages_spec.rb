dcf5c6e41437e542fff8ca35c34293d4276cac6a
biv <nicholas.faiz@gmail.com>
2011-01-27 04:44:41 +1100

    more work on archive_study. Need to revert to keep archive_to_study_integration_point, but also introduce archive_study

--- spec/acceptance/pages_spec.rb
+++ spec/acceptance/pages_spec.rb
@@ -2,11 +2,7 @@ require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature "serving out CMS pages" do
   
-<<<<<<< HEAD
-  scenario "requesting / should map to /ada/home" do
-=======
   scenario "requesting / should map to /ada_home" do
->>>>>>> parent of e26fbda... repairing test data - we create the ada home page in test data, everything is generated as needed by factories
     visit "/"
     page.status_code.should == 200
   end
