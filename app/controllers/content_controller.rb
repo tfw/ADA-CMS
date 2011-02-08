ae8474b2eb2b334a65cf272cad6b30d86282bcf1
biv <nicholas.faiz@gmail.com>
2011-02-08 11:24:19 +1100

    separate inkling management and cms implementation management completely. Home controller and base controller for staff

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,6 +1,8 @@
-class ContentController < Inkling::ContentController
+class ContentController < ApplicationController # Inkling::ContentController
   
-  # inkling_content_controller
+  layout 'content' #this file is created by the Theme functionality in admin, and written out to tmp/inkling/themes/layouts
+  alias current_user current_inkling_user
+  helper_method :current_user
   
   before_filter :get_archives
   before_filter :get_ada_pages
---
+++ app/controllers/staff/base_controller.rb
@@ -0,0 +1,5 @@
+
+class Staff::BaseController < Inkling::BaseController
+
+  layout 'inkling/manage'
+end
