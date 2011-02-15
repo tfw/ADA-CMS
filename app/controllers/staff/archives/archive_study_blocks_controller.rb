98a321a3d010296725f0e030db3badcc8ffe3fbb
biv <nicholas.faiz@gmail.com>
2011-02-15 22:34:10 +1100

    fixing inherited resources config for archive study blocks

--- app/controllers/staff/archives/archive_study_blocks_controller.rb
+++ app/controllers/staff/archives/archive_study_blocks_controller.rb
@@ -1,4 +1,4 @@
 class Staff::Archives::ArchiveStudyBlocksController < Staff::ArchivesController
   inherit_resources                                                                                     
-  defaults :resource_class => ArchiveStudyBlocks, :instance_name => 'archive_study_blocks'
+  defaults :resource_class => ArchiveStudyBlock, :instance_name => 'archive_study_blocks'
 end
