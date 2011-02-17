b5d9522dad713414c0ced36234921963fef9d49f
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-02-17 14:42:26 +1100

    spec for archive study blocks

--- app/controllers/staff/archives/archive_study_blocks_controller.rb
+++ app/controllers/staff/archives/archive_study_blocks_controller.rb
@@ -1,4 +1,30 @@
 class Staff::Archives::ArchiveStudyBlocksController < Staff::ArchivesController
   inherit_resources                                                                                     
-  defaults :resource_class => ArchiveStudyBlock, :instance_name => 'archive_study_blocks'
+  defaults :resource_class => ArchiveStudyBlock, :instance_name => 'archive_study_block'
+  before_filter :get_archive
+  
+  def create
+    create! do |format| 
+      format.html {
+        redirect_to archive_integrations_path(@archive)   
+        } 
+    end
+  end
+
+  def update
+    update! do |format| 
+      format.html {
+        redirect_to archive_integrations_path(@archive)   
+        } 
+    end
+  end
+
+  def destroy
+    destroy! do |format| 
+      format.html {
+        redirect_to archive_integrations_path(@archive)   
+        } 
+    end
+  end
+
 end
