e0570e44b496820243f71834e222cd7c70a3199b
Clifford Heath <clifford.heath@gmail.com>
2011-02-28 16:51:27 +1100

    Added News models

---
+++ app/models/news.rb
@@ -0,0 +1,11 @@
+class News < ActiveRecord::Base
+  has_many :news_archives
+  has_many :archive, :through => :news_archives
+  belongs_to :user, :class_name => "Inkling::User", :foreign_key => "user_id"
+
+  validates_presence_of :user
+  validates_presence_of :title
+  validates_presence_of :body
+  validates_presence_of :news_archives
+  validates_inclusion_of :state, :in => %w{draft published deleted}
+end
