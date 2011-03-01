2614fa2a9c63e959a1a243553a9ba7b56676eb4f
Clifford Heath <clifford.heath@gmail.com>
2011-03-01 17:23:10 +1100

    Roughed out the news edit screen

--- app/controllers/staff/news_controller.rb
+++ app/controllers/staff/news_controller.rb
@@ -2,11 +2,18 @@ class Staff::NewsController < Staff::BaseController
   inherit_resources                                                                                     
   defaults :resource_class => News, :instance_name => 'news'
 
+  before_filter :get_archives, :only => [:edit]
+
   def index
     get_recent_news
   end
   
+  private
   def get_recent_news
     @recent_news = News.find(:all, :limit => 10, :order => "created_at DESC")
+  end
+
+  def get_archives
+    @archives = Archive.all(:order => "name asc")
   end
 end
