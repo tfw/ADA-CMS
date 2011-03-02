3d5834ada49fbc6d84fa5069f2bd21520ecce9a8
Clifford Heath <clifford.heath@gmail.com>
2011-03-02 16:51:43 +1100

    Added custom validator that News is in at least one archive. Fixed News<->Archive assignment saving

--- app/controllers/staff/news_controller.rb
+++ app/controllers/staff/news_controller.rb
@@ -2,12 +2,20 @@ class Staff::NewsController < Staff::BaseController
   inherit_resources                                                                                     
   defaults :resource_class => News, :instance_name => 'news'
 
-  before_filter :get_archives, :only => [:edit]
+  before_filter :get_archives
 
   def index
     get_recent_news
   end
-  
+
+  def update
+    update! do |format| 
+      format.html {
+        redirect_to edit_staff_news_path(@news)
+      } 
+    end
+  end
+
   private
   def get_recent_news
     @recent_news = News.find(:all, :limit => 10, :order => "created_at DESC")
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,11 +1,12 @@
 class News < ActiveRecord::Base
   has_many :news_archives
-  has_many :archive, :through => :news_archives
+  has_many :archives, :through => :news_archives
+
   belongs_to :user, :class_name => "Inkling::User", :foreign_key => "user_id"
 
   validates_presence_of :user
   validates_presence_of :title
   validates_presence_of :body
-  #validates_presence_of :news_archives
+  validates_at_least_one :news_archives
   validates_inclusion_of :state, :in => %w{draft published deleted}
 end
