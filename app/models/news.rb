827553960e4fa4daae191326ff5a10dbae32561a
Clifford Heath <clifford.heath@gmail.com>
2011-03-02 17:17:42 +1100

    New news works, save news goes to preview (with edit link to return)

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,6 +7,6 @@ class News < ActiveRecord::Base
   validates_presence_of :user
   validates_presence_of :title
   validates_presence_of :body
-  validates_at_least_one :news_archives
+  #validates_at_least_one :news_archives
   validates_inclusion_of :state, :in => %w{draft published deleted}
 end
