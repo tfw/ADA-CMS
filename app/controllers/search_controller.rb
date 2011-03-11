4bf2170708ddb8127a395518cde4f747d500a05f
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-03-12 09:52:28 +1100

    move dashboard to balsamiq look and feel

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,12 +6,13 @@ class SearchController < ContentController
     @term = params[:term]
     @current_archive = Archive.find(params[:archive_id])
     search_params = {:page => params[:page], :match_mode => :any}
+    search_conditions = {}
     
     unless @current_archive == Archive.ada
-      search_params[:archive_id] = @current_archive.id
+      search_conditions[:archive_id] = @current_archive.id
     end
     
-    @sphinx = ThinkingSphinx.search(@term, search_params)
+    @sphinx = ThinkingSphinx.search(@term, search_params, :conditions => search_params)
 
     render :results
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,7 @@ module ApplicationHelper
   end
   
   def first_n_words(n, words)
+    return if words.nil?
     words = words.gsub(/<\/?[^>]*>/, "").split(/\W/m).reject{|w| w.empty? }
     words = words.size > n ? words[0...n]+['...'] : words
     words * ' '
