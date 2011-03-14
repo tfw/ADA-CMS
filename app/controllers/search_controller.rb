40b8db5529a44bd6b34cd07f6e410a458eb2e1de
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-03-14 18:52:14 +1100

    rudimentary faceting coming through, moving to branch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ContentController
     @current_archive = Archive.find(params[:archive_id])
   
     @sphinx = ThinkingSphinx.search(@term, :page => params[:page], :conditions => {:archive_id => @current_archive.id})
-
+    @facets = Study.facets @term, :conditions => {:archive_id => @current_archive.id}
     render :results
   end
 end
