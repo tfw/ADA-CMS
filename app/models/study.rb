5bb9d6a2ef68caa23fad6ec985d5c3ec6e437401
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-03-14 17:33:11 +1100

    repairing study spec - only load facet information if it exists in the hadh

--- app/models/study.rb
+++ app/models/study.rb
@@ -45,20 +45,29 @@ class Study < ActiveRecord::Base
     data.delete(:abstract)
     study.keywords = data[:keywords]
     data.delete(:keywords)
-    study.data_kind = data[:dataKind]
-    data.delete(:dataKind)
-    study.sampling = data[:sampling].split(/\n/).first
-    data.delete(:sampling)
-    study.collection_method = data[:collMode]
-    data.delete(:collMode)
 
+    #facet data
+    if data[:dataKind]      
+      study.data_kind = data[:dataKind]
+      data.delete(:dataKind)
+    end
+    
+    if data[:sampling]
+      study.sampling = data[:sampling].split(/\n/).first
+      data.delete(:sampling)
+    end
+    
+    if data[:collMode]
+      study.collection_method = data[:collMode]
+      data.delete(:collMode)
+    end
 
     study.save!
     local_data.each {|k,v| create_or_update_entry(study, k.to_s, v)}
 
     study
   end
-
+  
   def self.create_or_update_entry(study, key, value)
     begin
       study_field = StudyField.find_by_study_id_and_key(study.id, key)
