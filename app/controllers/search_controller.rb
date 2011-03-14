a21d708ea9b8decbb1b59753c2a9d5d4b329d37b
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-03-15 10:39:42 +1100

    prelim browse (facets) templating

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,22 +14,23 @@ class SearchController < ContentController
       Archive.qualitative => (Study.facets @term, :conditions => {:archive_id => Archive.qualitative.id}),
       Archive.international => (Study.facets @term, :conditions => {:archive_id => Archive.international.id})            
       }
+      
     render :results
   end
   
   def facets
     @current_archive = Archive.find(params[:archive_id])
-
+    @facet = params[:facet]
     @sphinx = ThinkingSphinx.facets(params[:facet], :page => params[:page], :conditions => {:archive_id => @current_archive.id})
   
-    @archive_facets = {Archive.social_science => (Study.facets params[:facet], :conditions => {:archive_id => Archive.social_science.id}),
-      Archive.historical => (Study.facets params[:facet], :conditions => {:archive_id => Archive.historical.id}),
-      Archive.indigenous => (Study.facets params[:facet], :conditions => {:archive_id => Archive.indigenous.id}),
-      Archive.longitudinal => (Study.facets params[:facet], :conditions => {:archive_id => Archive.longitudinal.id}),
-      Archive.qualitative => (Study.facets params[:facet], :conditions => {:archive_id => Archive.qualitative.id}),
-      Archive.international => (Study.facets params[:facet], :conditions => {:archive_id => Archive.international.id})            
+    @archive_facets = {Archive.social_science => (Study.facets @facet, :conditions => {:archive_id => Archive.social_science.id}),
+      Archive.historical => (Study.facets @facet, :conditions => {:archive_id => Archive.historical.id}),
+      Archive.indigenous => (Study.facets @facet, :conditions => {:archive_id => Archive.indigenous.id}),
+      Archive.longitudinal => (Study.facets @facet, :conditions => {:archive_id => Archive.longitudinal.id}),
+      Archive.qualitative => (Study.facets @facet, :conditions => {:archive_id => Archive.qualitative.id}),
+      Archive.international => (Study.facets @facet, :conditions => {:archive_id => Archive.international.id})            
       }
-      
-    render :results
+    
+    render :facets
   end
 end
