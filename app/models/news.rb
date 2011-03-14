d04908bb945fb9306d72ddd52b7fa522c7321814
Clifford Heath <clifford.heath@gmail.com>
2011-03-14 11:29:20 +1100

    Fixed named_scope deprecations

--- app/models/news.rb
+++ app/models/news.rb
@@ -11,8 +11,8 @@ class News < ActiveRecord::Base
   #validates_at_least_one :news_archives
   validates_inclusion_of :state, :in => %w{draft published deleted}
 
-  named_scope :recent, proc { { :limit => 10, :order => "created_at DESC" } }
-  named_scope :published, proc { { :conditions => "state = 'published'" } }
+  scope :recent, proc { { :limit => 10, :order => "created_at DESC" } }
+  scope :published, proc { { :conditions => "state = 'published'" } }
 
   def snippet
     words = body.gsub(/<\/?[^>]*>/, "").split(/\W/m).reject{|w| w.empty? }
