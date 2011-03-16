9d2ca28c6382656ac5cf45e3f7554e1ef8070d06
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-03-17 04:14:04 +1100

    touch the deploy log first

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,5 +67,6 @@ task :symlinks, :roles => :app do
 end
 
 task :deploy_log, :roles => :app do
-  system("echo \"Deployed at #{Time.now.strftime('%Y-%m-%d %I:%M')}\" > tmp/deploy-log.txt")
+  run "touch tmp/deploy-log.txt"
+  run "echo \"Deployed at #{Time.now.strftime('%Y-%m-%d %I:%M')}\" > tmp/deploy-log.txt"
 end
