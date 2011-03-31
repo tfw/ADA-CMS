cf7fd8c7a51162995753bde9231e4cbb4d8386ad
Clifford Heath <clifford.heath@gmail.com>
2011-03-31 14:12:45 +1100

    clicking site-resource document hyperlinks downloads the document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,8 +2,8 @@ class DocumentsController < ApplicationController
   respond_to :html
 
   def show
-    @document = Documents.find_by_id(params[:id])
-    respond_with(@document)
+    @document = Document.find_by_id(params[:id])
+    send_file @document.resource.path, :type => @document.resource.content_type
   end
 
 end
