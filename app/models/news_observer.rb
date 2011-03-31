94ddd40a14c4feb4a5ae9f5f38326a101e6f1175
Clifford Heath <clifford.heath@gmail.com>
2011-03-31 14:54:22 +1100

    Added news logging

---
+++ app/models/news_observer.rb
@@ -0,0 +1,36 @@
+class NewsObserver < ActiveRecord::Observer
+  include Rails.application.routes.url_helpers
+   
+  def after_create(news)
+    log("created", news) if news.created_at == news.updated_at
+  end
+
+  def after_save(news)
+    log("edited", news) if news.created_at != news.updated_at
+  end
+  
+  def after_destroy(news)
+    log("deleted", news)    
+  end
+  
+  private
+  def log(verb, news)
+    Inkling::Log.create!(
+      :text => "#{
+          news.user.email
+        } #{
+          verb
+        } news <a href='#{edit_staff_news_path(news)}'>#{news.title}</a>" +
+        "in #{
+          if (nas = news.news_archives).size == 0
+            "no archives"
+          else
+            nas.map do |na|
+              "<a href='#{na.path.slug}'>#{na.archive.name}</a>."
+            end*", "
+          end
+        }",
+      :category => "content"
+    )
+  end
+end
