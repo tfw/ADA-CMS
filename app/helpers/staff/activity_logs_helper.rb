ab8fa7eeee9bc4086212428bdcb39401a271ea10
biv <nicholas.faiz@gmail.com>
2011-04-04 16:20:12 +1000

    activity logs helper for icon for log

---
+++ app/helpers/staff/activity_logs_helper.rb
@@ -0,0 +1,14 @@
+module Staff::ActivityLogsHelper
+  
+  def log_icon(category)
+    case category
+    when "integration"
+      image_tag('icons/server.png')
+    when "study"
+      image_tag('icons/book_open.png')
+    when "users"
+      image_tag('icons/user.png')
+    end
+  end
+  
+end
