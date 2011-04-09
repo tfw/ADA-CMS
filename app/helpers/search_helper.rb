fd2f377739a2f461ce70c9073fbeaa5c32a3972f
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-04-09 16:30:36 +1000

    better formatting for vars

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -8,18 +8,16 @@ module SearchHelper
     highlights << "..."
     highlights.html_safe	
   end
-  
-  
-  def conceal?(format_name, format = "title")
-    return if format.nil? and format_name == "title"
-    "class = 'concealed'" unless format_name == format
+    
+  def conceal_unless(format_names, format = "title")
+    # debugger
+    return if format.nil? and format_names.include?("title")
+    "class = 'concealed'" unless format_names.include?(format)
   end
   
-  #the below is just bizarre.
   def selected(format_name, format = "title")
     css_class = nil
     css_class = "class = 'selected-menu-item'" if format_name == format or (format.nil? and format_name == "title") 
-    # "class = 'selected-menu-item'" if format.nil? and format_name == "title"  
     css_class
   end
 end
