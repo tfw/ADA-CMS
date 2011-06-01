cda4bcecd53529ae4faca5015d5da9623c41864c
biv <nicholas.faiz@gmail.com>
2011-06-01 21:24:28 +1000

    working on menuing for browse

--- app/controllers/archive_catalogs_controller.rb
+++ app/controllers/archive_catalogs_controller.rb
@@ -15,6 +15,11 @@ class ArchiveCatalogsController < ContentController
   
   protected
   def get_root_catalogs
-    @ada_catalogs = ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.ada.id, nil)
+    @ada_catalogs = {Archive.ada => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.ada.id, nil),
+    Archive.historical => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.historical.id, nil),
+    Archive.indigenous => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.indigenous.id, nil),
+    Archive.longitudinal => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.longitudinal.id, nil),
+    Archive.qualitative => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.qualitative.id, nil),
+    Archive.international => ArchiveCatalog.find_all_by_archive_id_and_parent_id(Archive.international.id, nil)}
   end  
 end
---
+++ app/helpers/archive_catalogs_helper.rb
@@ -0,0 +1,22 @@
+module ArchiveCatalogsHelper 
+  
+  def line_break_title(title, limit)
+    line_break = []
+    bits = title.split(/\W/) 
+
+    if bits.size > limit
+      i = 0      
+      for word in bits
+        line_break << word
+        i+= 1
+        if i == limit
+          line_break << "<br/>"
+          i = 0
+        end
+      end
+    else
+      title
+    end
+    line_break.join " "
+  end
+end
