b51e57c8edefb67649ff385e55ff1266e49c4f36
biv <nicholas.faiz@gmail.com>
2011-06-06 09:19:55 +1000

    moving to laptop

--- spec/acceptance/archive_catalogs_spec.rb
+++ spec/acceptance/archive_catalogs_spec.rb
@@ -2,9 +2,11 @@ require File.dirname(__FILE__) + '/acceptance_helper'
 
 feature "serving out Archive Catalogs" do
 
-
+  background do
+    ArchiveCatalog.make(:title => )
+  end
   
-  scenario "requesting / should map to /ada/home" do
+  scenario "requesting / should map to /ada/browse" do
     visit "/"
     page.status_code.should == 200
   end
