ba16bb23aa65ab07b7103ee997e16dfe8d97f374
biv <nicholas.faiz@gmail.com>
2011-06-09 22:26:13 +1000

    repairing models/archive_news_spec.rb

--- spec/models/archive_news_spec.rb
+++ spec/models/archive_news_spec.rb
@@ -5,10 +5,12 @@ describe ArchiveNews do
   
   describe "belonging to an archive" do
     specify "news which belong to archives take the archive name as a prefix, e.g. social-science/home" do
+      archive = Archive.make
       news = News.make
+      ArchiveNews.make(:news => news, :archive => archive)
       a = news.archives[0]
       a.should_not be_nil
-      na = news.news_archives[0]
+      na = news.archive_news[0]
       na.should_not be_nil
       na.path.slug.should =~ %r{/#{a.slug}/news/\d\d\d\d/\d\d/\d\d/#{sluggerize(news.title)}}
     end
