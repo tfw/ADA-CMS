935a04afbb3a3da1ede076bfcfa209021fc51513
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-06-16 16:57:45 +1000

    exclude vendor from simplecov

--- spec/acceptance/search_spec.rb
+++ spec/acceptance/search_spec.rb
@@ -20,24 +20,13 @@ feature "searching studies" do
   
   scenario "the search form should lead to the search results page" do
     visit "/"
-    search_form(Study.first.label.split(/\w/).first)
+    page = search_form(Study.first.label.split(/\w/).first, page)
     page.status_code.should == 200
   end
   
   scenario "it should default to a studies title view if the format http param isnt set" do
-    search_form("foo")
+    page = search_form("foo", page)
     page.should have_content("TITLE")    
-  end
-  
-  scenario "the view http param should define whether jquery renders onload the title view (study), extended view (study, or variables)" do
-    search("foo", "title")
-    # puts page.body
-    page.should have_content("TITLE")
-    search("foo", "ext")
-    page.should have_content("EXTENDED")
-    search("foo", "var")    
-    page.should_not have_content("EXTENDED")
-    page.should_not  have_content("TITLE")
   end
   
   scenario "a save search form should be available (if there is a current_user)" do
