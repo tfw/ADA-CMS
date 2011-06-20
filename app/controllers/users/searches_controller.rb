ece9f7fa1d2abc99165d7ee0dfa6dc6e46e605d0
biv <nicholas.faiz@gmail.com>
2011-06-20 21:15:59 +1000

    basic ordering (with sql injection protection) for saved search

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -3,8 +3,17 @@ class Users::SearchesController < ContentController
   defaults :resource_class => Search, :instance_name => 'search'
   
   def index
+    order_by = params[:sort] if Search.column_names.include?(params[:sort])
+    order = params[:order]
+    
+    if params[:order] == "up"
+      order = "ASC" 
+    elsif params[:order] == "down"
+      order =  "DESC"
+    end
+    
     @current_archive = Archive.ada
-    @searches = current_user.searches
+    @searches = current_user.searches.order("#{order_by} #{order}")
   end
   
   def destroy
