5518afb4ee2051b3aa58859fd2858ab0f8bef0fe
biv <nicholas.faiz@gmail.com>
2011-07-13 14:59:43 +1000

    menu items can have parents in form

--- app/controllers/staff/archives/menu_items_controller.rb
+++ app/controllers/staff/archives/menu_items_controller.rb
@@ -2,6 +2,7 @@ class Staff::Archives::MenuItemsController < Staff::Archives::BaseController
   inherit_resources
   defaults :resource_class => MenuItem, :instance_name => 'menu_item'
   before_filter :get_archive
+  before_filter :get_menu_items, :except => :destroy  
   
   
   def create
