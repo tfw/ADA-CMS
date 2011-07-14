cdc5242e8a9f8bd98764bd39188b4483e909a10e
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-07-14 11:16:52 +1000

    Do not display partner logos unless on a subarchive home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     archive.name == "ADA" ? 'default' : archive.name.gsub(" ", "").underscore
   end
 
+  def subarchive_home_page?
+    @page.try(:title) == "Home" and @current_archive != Archive.ada
+  end
+
   def nesstar_link(dataset)
     study_id = dataset.about.split(".").last
     return "http://assda-nesstar.anu.edu.au/webview/index.jsp?study=http%3A%2F%2Fassda-nesstar.anu.edu.au%3A80%2Fobj%2FfStudy%2Fau.edu.anu.assda.ddi.#{study_id}&amp;v=2&amp;mode=documentation&amp;submode=abstract&amp;top=yes"
