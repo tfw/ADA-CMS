ea9371abd04a35db6774b0f5ea96890d532b704d
biv <nicholas.faiz@gmail.com>
2011-07-26 13:41:36 +1000

    use simplest practice for js and css concatenation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,6 @@ module ApplicationHelper
     @page.try(:title) == "Home"
   end
 
-  def nesstar_link(dataset)
-    study_id = dataset.about.split(".").last
-    return "http://assda-nesstar.anu.edu.au/webview/index.jsp?study=http%3A%2F%2Fassda-nesstar.anu.edu.au%3A80%2Fobj%2FfStudy%2Fau.edu.anu.assda.ddi.#{study_id}&amp;v=2&amp;mode=documentation&amp;submode=abstract&amp;top=yes"
-  end
-
   def news_snippet(news)
     first_n_words(20, news.body)
   end
