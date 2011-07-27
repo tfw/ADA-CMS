1e901af87da37a1ed9404f89e72b4fd93e6287f2
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-07-27 11:47:05 +1000

    Fixed current navigation highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,11 @@ module ApplicationHelper
     @page.try(:title) == "Home"
   end
 
+  # Returns the class (either "current" or "") for a menu item in the primary navigation
+  def menu_item_class(menu_item_title)
+    @current_archive == Archive.ada && menu_item_title == @title ? "current" : ""
+  end
+
   def nesstar_link(dataset)
     study_id = dataset.about.split(".").last
     return "http://assda-nesstar.anu.edu.au/webview/index.jsp?study=http%3A%2F%2Fassda-nesstar.anu.edu.au%3A80%2Fobj%2FfStudy%2Fau.edu.anu.assda.ddi.#{study_id}&amp;v=2&amp;mode=documentation&amp;submode=abstract&amp;top=yes"
