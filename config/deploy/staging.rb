532e7514264ba3d4e93cfedf0f60db241c5f22ef
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-08-02 17:03:55 +1000

    Using the IP address for deploying to web5 for now.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,9 +3,9 @@ require 'rvm/capistrano'
 
 set :rvm_ruby_string, 'ruby-1.9.2-p290'
 
-role :web, "web5.nci.org.au"
-role :app, "web5.nci.org.au"
-role :db,  "web5.nci.org.au", :primary => true
+role :web, "150.203.254.162"
+role :app, "150.203.254.162"
+role :db,  "150.203.254.162", :primary => true
 
 set :user,        "d10web"
 set :use_sudo,    false
