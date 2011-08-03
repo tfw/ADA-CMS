a33b9fb884f42d5f8741b41f585108a2ebdba3c3
biv <nicholas.faiz@gmail.com>
2011-08-03 12:21:39 +1000

    working on authorization for CMS:

--- app/controllers/staff/base_controller.rb
+++ app/controllers/staff/base_controller.rb
@@ -1,11 +1,25 @@
 class Staff::BaseController < ApplicationController
 
+  PERMITTED_ROLES = [Inkling::Role.find_by_name("administrator"),
+  					Inkling::Role.find_by_name("Manager"),
+  					Inkling::Role.find_by_name("Approver"),
+  					Inkling::Role.find_by_name("Archivist")]
+
   helper_method :current_user
   before_filter :authenticate_user!
+  before_filter :redirect_if_unauthorised
   
   layout 'manage'
   
   def devise_controller?
     true
+  end
+
+  def redirect_if_unauthorised
+  	puts "------------"
+  	debugger
+  	unless (current_user.roles & PERMITTED_ROLES).any?
+  		redirect_to root_path
+  	end
   end
 end
