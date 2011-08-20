0e3bc6bf4d40c10b0fde0427e07366d7b0ad2638
biv <nicholas.faiz@gmail.com>
2011-08-20 13:41:06 +1000

    adding google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,4 +47,10 @@ module ApplicationHelper
   def variable_anchor(variable)
     "#{variable.id}"
   end
+
+  def google_analytics
+    %{<script type="text/javascript">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-25243708-1']);_gaq.push(['_setDomainName', '.ada.edu.au']);_gaq.push(['_trackPageview']);
+ (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
+  </script>}.html_safe
+  end
 end
