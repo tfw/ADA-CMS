82623bcdb809e27f60afb258a5ad483ed4a41292
biv <nicholas.faiz@gmail.com>
2011-08-23 10:58:08 +1000

    administrators cannot login - we know why

--- app/controllers/staff/base_controller.rb
+++ app/controllers/staff/base_controller.rb
@@ -19,8 +19,7 @@ class Staff::BaseController < ApplicationController
 
   private
   def permitted_roles
-    [Inkling::Role.find_by_name("administrator"),
-      Inkling::Role.find_by_name("Manager"),
+    [Inkling::Role.find_by_name("Manager"),
       Inkling::Role.find_by_name("Publisher"),
       Inkling::Role.find_by_name("Approver"),
       Inkling::Role.find_by_name("Archivist")]
