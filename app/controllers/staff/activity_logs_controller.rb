e55db4d0570d7d0364acce1d14fb5411b665d515
Nicholas <nicholas.faiz@gmail.com>
2011-09-02 09:49:02 +1000

    dashboard UI looking a bit better - logs refactored to match look of approvals

--- app/controllers/staff/activity_logs_controller.rb
+++ app/controllers/staff/activity_logs_controller.rb
@@ -5,6 +5,12 @@ class Staff::ActivityLogsController < Staff::BaseController
   layout false
   
   def index
-    @logs = Inkling::Log.find(:all, :limit => 20, :order => "created_at DESC")
+    logs = Inkling::Log.find(:all, :limit => 40, :order => "created_at DESC")
+    unless logs.length < 20
+	    @first_20 = logs[0..19]
+	    @second_20 = logs [20..-1]
+	else
+		@first20 = logs[0..19]
+	end
   end
 end
--- app/controllers/staff/home_controller.rb
+++ app/controllers/staff/home_controller.rb
@@ -1,9 +1,11 @@
 class Staff::HomeController < Staff::BaseController
   
   def dashboard
-  	@draft_news = News.where("state ='draft'").order("updated_at asc").limit(20);
+  	pages_limit = params[:all_pages] ? nil : 20;
+  	news_limit = params[:all_news] ? nil : 20; 
+  	@draft_news = News.where("state ='draft'").order("updated_at asc").limit(news_limit);
   	@draft_news_count = News.where("state ='draft'").order("updated_at asc").count;
-  	@draft_pages = Page.where("state ='draft'").order("updated_at asc").limit(20);
+  	@draft_pages = Page.where("state ='draft'").order("updated_at asc").limit(pages_limit);
   	@draft_pages_count = Page.where("state ='draft'").order("updated_at asc").count;
   end
 end
