7d41a50a5902cbc875d4b08c12388cd35aba1027
Nicholas <nicholas.faiz@gmail.com>
2011-09-01 16:21:09 +1000

    finalizing UI for content publishing approval

--- app/controllers/staff/home_controller.rb
+++ app/controllers/staff/home_controller.rb
@@ -1,7 +1,9 @@
 class Staff::HomeController < Staff::BaseController
   
   def dashboard
-  	@draft_news = News.where("state ='draft'").order("updated_at desc");
-  	@draft_pages = Page.where("state ='draft'").order("updated_at desc");
+  	@draft_news = News.where("state ='draft'").order("updated_at asc").limit(20);
+  	@draft_news_count = News.where("state ='draft'").order("updated_at asc").count;
+  	@draft_pages = Page.where("state ='draft'").order("updated_at asc").limit(20);
+  	@draft_pages_count = Page.where("state ='draft'").order("updated_at asc").count;
   end
 end
